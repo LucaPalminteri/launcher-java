0567cb05f78c11770130cb711ec7810af9c74f6f
LucaPalminteri <lucapalminteri02@gmail.com>
2022-08-26 17:15:41 -0300

    first commit

---
+++ app/src/main/java/com/example/applicationone/Item.java
@@ -0,0 +1,9 @@
+package com.example.applicationone;
+
+import android.graphics.drawable.Drawable;
+
+public class Item {
+    CharSequence label; // package name
+    CharSequence name; // app name
+    Drawable icon; // app icon
+}
